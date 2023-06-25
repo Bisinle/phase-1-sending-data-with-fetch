da594bafaf7662874729cfbb49be287346f40325
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-06-25 17:59:58 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email,
+  };
+
+  let config = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", config)
+    .then((response) => response.json())
+    .then((data) => {
+      document.body.innerHTML += `<p>New ID: ${data.id}</p>`;
+    })
+    .catch((error) => {
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
+
+// Testing the submitData function
+submitData("John Doe", "johndoe@example.com");
